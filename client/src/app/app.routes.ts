f72ea5bbb6a9f908ece8c2bc9835ca5ec504ac65
farrahalyssa <idcfarrah@gmail.com>
2025-01-28 15:38:59 -0500

    initialize project

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path: '', component: RegistrationComponent},
+    {path: 'home', component:HomeComponent},
+    {path: 'login', component:LoginComponent},
+    { path: '**', redirectTo: 'home' }  
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
